a40f7ce6246fc3a41f64a7a60299349103588930
Shelby Goudy <shelbygoudy@gmail.com>
2020-08-13 11:30:52 -0400

    Step 8, update Course routes done.. must update user routes

---
+++ courses.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+
+function generateRandomId(){
+    return Math.floor(Math.random() * 10000);
+  }
+
+function save(data){
+  return new Promise((resolve, reject) => {
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * Gets all courses
+ * @param None
+ */
+function getCourses(){
+  return new Promise((resolve, reject) => {
+    fs.readFile('./seed/data.json', 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const json = JSON.parse(data);
+        resolve(json);
+      }
+    });
+  });
+}
+
+/**
+ * Gets a specific course by ID
+ * @param {number} id - Accepts the ID of the specified course.
+ */
+async function getCourse(id){
+  const courses = await getCourses();
+  return courses.courses.find(course => course.id == id);
+}
+
+
+/**
+ * Creates a new quote record 
+ * @param {Object} newCourse- Object containing info for new quote: the quote text, author and year 
+ */
+async function createCourse(newCourse) {
+  const courses = await getCourses(); 
+  
+  newCourse.id = generateRandomId(); 
+  courses.courses.push(newRecord);
+  await save(courses); 
+  return newCourse; 
+}
+
+/**
+ * Updates a single record 
+ * @param {Object} newCourse - An object containing the changes to quote: quote, author, year (all optional)
+ */
+async function updateCourse(newCourse){
+  const courses = await getCourses();
+  let course = courses.courses.find(item => item.id == newCourse.id);
+  
+  course.title = newCourse.title;
+  course.description = newCourse.description;
+  course.estimatedTime = newCourse.estimatedTime;
+  course.materialsNeeded = newCourse.materialsNeeded;
+ 
+  await save(courses);
+}
+
+/**
+ * Deletes a single record
+ * @param {Object} course - Accepts record to be deleted. 
+ */
+async function deleteCourse(course){
+  const courses = await getCourses();
+  courses.courses = courses.courses.filter(item => item.id != course.id);
+  await save(courses);
+}
+
+module.exports = {
+  getCourses,
+  getCourse, 
+  createCourse, 
+  updateCourse, 
+  deleteCourse,
+  generateRandomId
+}
--- routes.js
+++ routes.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const express = require('express');
+const courses = require('./courses');
 
-// This array is used to keep track of user records
-// as they are created.
+// Refactoring to reduce code repetition
+function asyncHandler(cb){
+    return async (req, res, next)=>{
+      try {
+        await cb(req,res, next);
+      } catch(err){
+        next(err);
+      }
+    }
+  }
+
 const users = [];
 
 // Construct a router instance.
@@ -24,12 +34,75 @@ router.post('/users', (req, res) => {
     res.status(201).end();
 });
 
+
+
 //------- COURSE routes-----------//
 
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
+router.get('/courses', asyncHandler(async(req,res)=>{
+    const courses = await courses.getCourses();
+    res.json(courses);
+    res.json({message: err.message});
+}));
+
+
+
 // GET /api/courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
+router.get('/courses/:id', asyncHandler( async(req, res)=>{
+    const id = req.params.id;
+    const course = await courses.getCourses(id);
+    if(course){
+      res.json(course);
+    } else {
+      res.status(404).json({message: "Course not found."})
+    }
+    res.status(500).json({message: err.message})
+}));
+
+
+
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
+router.post('/courses', asyncHandler(async (req,res)=>{
+    if(req.body.title && req.body.description){
+      const course = await courses.createCourse({
+        title: req.body.title,
+        description: req.body.description
+      });
+      res.status(201).json(course);
+    } else {
+      res.status(400).json({message: "Title and description required."})
+    }
+  }));
+
+
 // PUT /api/courses/:id 204 - Updates a course and returns no content
+router.put('/courses/:id', asyncHandler (async(req,res)=> {
+    const course = await courses.getCourse(req.params.id);
+  if (course){
+    course.title = req.body.title;
+    course.description = req.body.description;
+
+    await courses.updateCourse(course);
+    res.status(204).end();
+  } else {
+    res.status(404).json({message: "Course Not Found."})
+  }
+    res.status(500).json({message: err.message})
+}));
+
+
 // DELETE /api/courses/:id 204 - Deletes a course and returns no content
+router.delete('/courses/:id', asyncHandler (async(req,res)=>{ 
+    const course = await courses.getCourse(req.params.id);
+  if(course){
+    await courses.deleteCourse(course);
+    res.status(204).end();
+    } else {
+      res.status(404).json({message: "Course Not Found."})
+    }
+    res.status(500).json({message: err.message})
+}));
+
+
 
 module.exports = router;
