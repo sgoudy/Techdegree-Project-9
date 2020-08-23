2eb858ac24eec730af03a96c07d690093fb9bf20
Shelby Goudy <shelbygoudy@gmail.com>
2020-08-23 13:23:50 -0400

    Ran seed and final

--- routes.js
+++ routes.js
@@ -13,54 +13,53 @@ const Course = require('./models').Course;
 
 // Async Try / Catch block for global error handler
 function asyncHandler(cb){
-    return async (req, res, next)=>{
-      try {
-        await cb(req, res, next);
-      } catch(err){
-        next(err);
-      }
+  return async (req, res, next)=>{
+    try {
+      await cb(req, res, next);
+    } catch(err){
+      next(err);
     }
+  }
 };
 
 /**
- * Authenticate User using 'basic-auth'
+ * Authenticate User using 'basic-auth' and 'bcrypt'
  */
 
 const authenticateUser = async (req, res, next) => {
-    let message = null;
-    let user;
-  // Parse Credentials from Header
-    const credentials = auth(req);
-
-    if (credentials) {
-      user = await User.findOne({
-        attributes: ['id', 'firstName', 'lastName', 'emailAddress', 'password'],
-        where: {
-          emailAddress: credentials.name
-        }
-      })
-        if (user){
-          const authenticated = bcrypt.compareSync(credentials.pass, user.password);
-          if (authenticated){
-            console.log(`Authentication successful for: ${credentials.name}`);
-            req.currentUser = user;
-          } else {
-            message = `Authentication failure for username: ${user.username}`;
-          }
-        } else {
-            message = `User not found for: ${credentials.name}`
-        }       
+  let message = null;
+  let user;
+// Parse Credentials from Header
+  const credentials = auth(req);
+  if (credentials) {
+    user = await User.findOne({
+      attributes: ['id', 'firstName', 'lastName', 'emailAddress', 'password'],
+      where: {
+        emailAddress: credentials.name
+      }
+    });
+    if (user){
+      const authenticated = bcrypt.compareSync(credentials.pass, user.password);
+      if (authenticated){
+        console.log(`Authentication successful for: ${credentials.name}`);
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
     } else {
-      message = `Auth header not found`;
-    }
-  //FAILED AUTH
-    if (message) {
-      console.warn(message);
-      res.status(401).json({ message: 'Access Denied' });
-  // SUCCESS
-    } else {
-      next();
-    }
+        message = `User not found for: ${credentials.name}`;
+    }       
+  } else {
+    message = `Auth header not found`;
+  }
+//FAILED AUTH
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+// SUCCESS
+  } else {
+    next();
+  }
 };
   
   
@@ -69,7 +68,6 @@ const authenticateUser = async (req, res, next) => {
 // GET /api/users 200 - Returns the currently authenticated user
 router.get('/users', authenticateUser, async (req, res) => {
   const user = req.currentUser;
-  console.log(user)
   res.json({
     User_ID: user.id,
     First_Name: user.firstName,
@@ -95,8 +93,8 @@ router.post('/users', [
   check('password')
     .exists({ checkNull: true, checkFalsy: true })
     .withMessage('Please provide a value for "password"'),
-], async(req, res) => {
-// Good request, hash password
+], asyncHandler (async(req, res) => {
+  // Good request, hash password
   try{
     await User.create({
       firstName: req.body.firstName, 
@@ -105,7 +103,6 @@ router.post('/users', [
       password: await bcrypt.hash(req.body.password, 10)
       });
     return res.status(201).redirect('/');
-
   } catch (error){
     // Validation Errors    
     const errors = validationResult(req);
@@ -118,75 +115,74 @@ router.post('/users', [
       return res.status(400).json({message: 'Email in use.'})
     }   
   } 
-});
+}));
 
 
   //------- COURSE routes-----------//
 
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
 router.get('/courses', asyncHandler ( async (req,res)=>{
-    const courses = await Course.findAll({
-      attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId']
-    });
-    res.json(courses);
+  const courses = await Course.findAll({
+    attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId']
+  });
+  res.json(courses);
 }));
 
 
 // GET /api/courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
 router.get('/courses/:id', asyncHandler( async(req, res)=>{
-    const id = req.params.id;
-    const course = await Course.findOne({
-      attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
-    where: {
-      id: id
-    }});
-    if (course) {
-      res.json(course);
-    } else {
-      res.status(404).json({message: "Course not found."})
-    }
+  const id = req.params.id;
+  const course = await Course.findOne({
+    attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
+  where: {
+    id: id
+  }});
+  if (course) {
+    res.json(course);
+  } else {
+    res.status(404).json({message: "Course not found."})
+  }
 }));
 
 
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
 router.post('/courses', authenticateUser, [
   check('title')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Please provide a value for "title"'),
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "title"'),
   check('description')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Please provide a value for "description"'),
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "description"'),
   ], asyncHandler (async (req, res, next) => {
   // Attempt to get the validation result from the Request object.
   const errors = validationResult(req);
   // If there are validation errors...
   if (!errors.isEmpty()) {
-      // Use the Array `map()` method to get a list of error messages.
-      const errorMessages = errors.array().map(error => error.msg);
-      // Return the validation errors to the client.
-      return res.status(400).json({ errors: errorMessages });
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
+    // Return the validation errors to the client.
+    return res.status(400).json({ errors: errorMessages });
   }
   const course = await Course.create({
-      id: req.body.id,
-      userId: req.body.userId,
-      title: req.body.title,
-      description: req.body.description,
-      estimatedTime: req.body.estimatedTime,
-      materialsNeeded: req.body.materialsNeeded
+    id: req.body.id,
+    userId: req.body.userId,
+    title: req.body.title,
+    description: req.body.description,
+    estimatedTime: req.body.estimatedTime,
+    materialsNeeded: req.body.materialsNeeded
   });
   res.status(201).json(course);
 }));
 
 
 // PUT /api/courses/:id 204 - Updates a course and returns no content
-// TODO provide min data req
 router.put('/courses/:id', authenticateUser, [
   check('title')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Please provide a value for "title"'),
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "title"'),
   check('description')
-      .exists({ checkNull: true, checkFalsy: true })
-      .withMessage('Please provide a value for "description"'),
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "description"'),
   ], asyncHandler (async(req,res, next)=> {
   const id = req.params.id; 
   const owner = req.currentUser.id; 
